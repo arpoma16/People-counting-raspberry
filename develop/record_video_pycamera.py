fca4343137a78b651700304a56fea45a85c5b744
Alvaro Ramiro Poma Aguilar <arpoma167@gmail.com>
2023-01-03 19:22:38 +0100

    add record files

---
+++ develop/record_video_pycamera.py
@@ -0,0 +1,38 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+ 
+# inicializa la cámara
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+camera.rotation = 180
+camera.hflip = True
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# espera un tiempo a aque la cámara esté lista
+time.sleep(0.1)
+
+
+writer= cv2.VideoWriter('/home/pi/people_counting.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
+
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # tomamos el array de numpy que reprsenta la image
+    image = frame.array
+	# muestra el frame
+    cv2.imshow("Frame", image)
+
+    writer.write(frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+writer.release()
+camera.close()
+cv2.destroyAllWindows()
+
+#if __name__ == '__main__':
+    
+
